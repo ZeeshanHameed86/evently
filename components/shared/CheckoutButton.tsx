05168c5692f6c6db182547f5320c31526b0934f8
Zeeshan Hameed <93074888+ZeeshanHameed86@users.noreply.github.com>
2024-06-08 22:43:24 +0500

    test

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,13 +1,14 @@
-"use client"
+"use client";
 
-import { IEvent } from '@/lib/database/models/event.model'
-import { SignedIn, SignedOut, useUser } from '@clerk/nextjs'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from '../ui/button'
-import Checkout from './Checkout'
+import { IEvent } from "@/lib/database/models/event.model";
+import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import Checkout from "./Checkout";
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
+  console.log({ event }, "2");
   const { user } = useUser();
   const userId = user?.publicMetadata.userId as string;
   const hasEventFinished = new Date(event.endDateTime) < new Date();
@@ -15,14 +16,14 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
   return (
     <div className="flex items-center gap-3">
       {hasEventFinished ? (
-        <p className="p-2 text-red-400">Sorry, tickets are no longer available.</p>
-      ): (
+        <p className="p-2 text-red-400">
+          Sorry, tickets are no longer available.
+        </p>
+      ) : (
         <>
           <SignedOut>
             <Button asChild className="button rounded-full" size="lg">
-              <Link href="/sign-in">
-                Get Tickets
-              </Link>
+              <Link href="/sign-in">Get Tickets</Link>
             </Button>
           </SignedOut>
 
@@ -32,7 +33,7 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default CheckoutButton
+export default CheckoutButton;
